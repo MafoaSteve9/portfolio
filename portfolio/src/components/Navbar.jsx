d6978110fdb2a907e90161ce3f0a7c5f0fee3213
Mafoa <steve_mafoa@outlook.com>
2023-11-28 14:34:11 +0100

    Modification de la navbar

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,25 +1,44 @@
 import { useState } from "react";
 import hamburger from "../assets/hamburger.svg"
 import close from "../assets/close.svg"
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { RiContactsBook2Fill } from "react-icons/ri";
 
 export default function Navbar() {
     const [showMenu, setShowMenu] = useState(false)
 
   return (
-    <nav className="fixed top-0 w-full flex justify-center p-4 bg-slate-200">
-        <ul className={`${showMenu ? "flex" : "hidden"} flex-col items-center bg-slate-200 w-full absolute top-full pb-5 sm:flex sm:relative sm:flex-row sm:pb-0 sm:justify-center`}>
-            <li className="font-semibold text-slate-800">
+    <nav className="fixed top-0 w-full flex justify-center p-4 bg-black text-slate-200">
+        <ul className={`${showMenu ? "flex" : "hidden"} flex-col items-center w-full absolute top-full pb-5 sm:flex sm:relative sm:flex-row sm:pb-0 gap-10 sm:justify-between`}>
+       <div>
+       <h1 className="italic font-semibold text-lg">Steve Mafoa</h1>
+       </div>
+            <div className="flex  sm:flex-row ">
+            <li className="font-semibold text-slate-50">
                 <a className="inline-block py-2 mx-3 text-lg sm:py-0" href="">Projets</a>
             </li>
-            <li className="font-semibold text-slate-800">
+            <li className="font-semibold text-slate-50">
                 <a className="inline-block py-2 mx-3 text-lg sm:py-0" href="">Moi</a>
             </li>
-            <li className="font-semibold text-slate-800">
+            <li className="font-semibold text-slate-50">
                 <a className="inline-block py-2 mx-3 text-lg sm:py-0" href="">Parcours</a>
             </li>
-            <li className="font-semibold text-slate-800">
+            <li className="font-semibold text-slate-50">
                 <a className="inline-block py-2 mx-3 text-lg sm:py-0" href="">Contact</a>
             </li>
+            </div>
+            <div className="flex  sm:flex-row">
+            <li className="font-semibold text-slate-50">
+                <a className="inline-block py-2 mx-3 text-lg sm:py-0 text-blue-500" href=""><FaLinkedin /></a>
+            </li>
+            <li className="font-semibold text-slate-50">
+                <a className="inline-block py-2 mx-3 text-lg sm:py-0" href=""><FaGithub /></a>
+            </li>
+            <li className="font-semibold text-slate-50">
+                <a className="inline-block py-2 mx-3 text-lg sm:py-0" href=""><RiContactsBook2Fill /></a>
+            </li>
+            </div>
         </ul>
         <button
         onClick={() => setShowMenu(!showMenu)}
@@ -29,5 +48,6 @@ export default function Navbar() {
             src={showMenu ? close : hamburger} alt={showMenu ?  "Cacher le menu": "Montrer le menu"} />
         </button>
     </nav>
+    
   )
 }
