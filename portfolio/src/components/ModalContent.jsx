e751fbde674db0cffe2a802ad379c4870c80ff21
Mafoa <steve_mafoa@outlook.com>
2023-11-30 12:41:06 +0100

    Creation de la fenetre modal avec un portal

---
+++ portfolio/src/components/ModalContent.jsx
@@ -0,0 +1,35 @@
+export default function ModalContent({closeModal}) {
+    const data = [
+        {
+            id:1,
+            infos: "steve_mafoa@outlook.com",
+        },
+        {
+            id:2,
+            infos: "06 58 43 81 62",
+        },
+    ]
+  return (
+    <div 
+    className="fixed inset-0 bg-slate-800/75 flex items-center justify-center"
+    onClick={closeModal}>
+        <div
+        className="bg-slate-300 text-slate-900 p-10 rounded relative mb-[10vh]" 
+        onClick={e => e.stopPropagation()}>
+            <button 
+            className="absolute top-1 right-1 w-7 h-7 bg-red-600 text-slate-100 rounded flex justify-center items-center"
+            onClick={closeModal}>X</button>
+            <p className="sm:w-[300px] pb-2">Bonjour, je recherche une alternance de 12 mois débutant à partir d’octobre 2023 en tant que développeur rythme 4j entreprise et 1j formation</p>
+            <ul>
+                <h1 className="font-semibold">Me joindre :</h1>
+                {data.map(info => (
+                    <li className="" key={info.id}>{info.infos}</li>
+                ))}
+
+            </ul>
+
+        </div>
+
+    </div>
+  )
+}
