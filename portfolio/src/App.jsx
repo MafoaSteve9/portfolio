acea364cd6b105db33ad7ff45b4b14048718bfe7
Mafoa <steve_mafoa@outlook.com>
2023-12-07 14:42:33 +0100

    Mise en place des routes avec React router-dom

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,38 @@
 import Navbar from './components/Navbar'
 import Presentation from './components/Presentation'
+import Projets from './components/Projets'
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import MesProjets from './pages/MesProjets'
+import Parimis from './pages/Parimis'
+import BoxyGenerator from './pages/BoxyGenerator'
+import Porsche from './pages/Porsche'
+import Trello from './pages/Trello'
+
 
 function App() {
   return (
-    <div>
+    <>
+
+     <BrowserRouter>
      <Navbar />
      <Presentation />
-    </div>
+     <Projets />
+     <MesProjets />
+
+     <Routes>
+     <Route path="/"/>
+          
+            <Route path="/pages/parimis" element={<Parimis />} />
+            <Route path="/pages/boxygenerator" element={<BoxyGenerator />} />
+            <Route path="/pages/porsche" element={<Porsche />} />
+            <Route path="/pages/trello" element={<Trello />} />
+         
+     </Routes>
+
+     </BrowserRouter>
+
+    </>
   )
 }
 
