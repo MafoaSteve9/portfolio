be387cd024169a62ffcc45b0c920b57c270bb1d4
Mafoa <steve_mafoa@outlook.com>
2023-12-07 14:43:33 +0100

    Modification de la width de la modal

--- portfolio/src/components/ModalContent.jsx
+++ portfolio/src/components/ModalContent.jsx
@@ -19,7 +19,7 @@ export default function ModalContent({closeModal}) {
             <button 
             className="absolute top-1 right-1 w-7 h-7 bg-red-600 text-slate-100 rounded flex justify-center items-center"
             onClick={closeModal}>X</button>
-            <p className="sm:w-[300px] pb-2">Bonjour, je recherche une alternance de 12 mois débutant à partir d’octobre 2023 en tant que développeur rythme 4j entreprise et 1j formation</p>
+            <p className="sm:w-[600px] pb-2">Bonjour, je recherche une alternance de 12 mois débutant à partir d’octobre 2023 en tant que développeur rythme 4j entreprise et 1j formation</p>
             <ul>
                 <h1 className="font-semibold">Me joindre :</h1>
                 {data.map(info => (
