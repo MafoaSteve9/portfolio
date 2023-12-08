3b8956f9a6cb5caab49e2830ca14ef9ae07f86e4
Mafoa <steve_mafoa@outlook.com>
2023-12-08 17:49:44 +0100

    Modification du component

--- portfolio/src/pages/Porsche.jsx
+++ portfolio/src/pages/Porsche.jsx
@@ -3,8 +3,8 @@ import porsche from "../assets/porsche.png"
 export default function Porsche() {
   return (
     <section>
-        <div className="flex justify-center p-10">
-        <p className="p-10 text-justify  w-[900px] bg-orange-50 rounded-lg text-[#666666] leading-8 shadow-2xl shadow-slate-400" >
+        <div className="flex justify-center p-6">
+        <p className="p-4 sm:p-10 sm:text-justify  w-[900px] bg-orange-50 rounded-lg text-[#666666] leading-8 shadow-2xl shadow-slate-400" >
         <h1 className="text-center uppercase font-bold text-lg p-4 text-black">Porsche</h1>
         Explorez l'élégance intemporelle de Porsche à travers notre site de photos minimaliste. Réalisé en HTML, CSS et JS, découvrez une collection captivante de clichés mettant en valeur la beauté des voitures Porsche. Cliquez sur le lien pour une expérience immersive et pour découvrir davantage de clichés exclusifs.
 
@@ -12,7 +12,7 @@ export default function Porsche() {
         </div>
         <div className="flex flex-wrap items-center mt-3 justify-center text-center flex-col sm:flex-row">
         <div className=" w-full sm:w-1/2 p-8">
-                    <a href="https://porsche-ms.vercel.app/" target="_blank" className="sm:relative sm:top-60 relative top-44 py-3 px-10 cursor-pointer text-sm rounded bg-[#65749280] text-white hover:bg-blue-700 opacity-75 font-semibold">Découvrir</a>
+                    <a href="https://porsche-ms.vercel.app/" target="_blank" className="sm:relative sm:top-60 relative top-24 text-[12px] py-3 px-10 cursor-pointer text-sm rounded bg-[#65749280] text-white hover:bg-blue-700 opacity-75 font-semibold">Découvrir</a>
                     <img className=" bg-slate-500 shadow-2xl shadow-slate-400 rounded-lg" src={porsche} alt="Photos de voiture de porsche" />
                 </div>
         </div>
