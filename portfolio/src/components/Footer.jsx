909285ecacc634a5c69259c37fa31f80a72a5fa1
Mafoa <steve_mafoa@outlook.com>
2023-12-08 18:22:36 +0100

    ajout du cv

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
        <ul className="flex items-center justify-center pt-8">
            <li className=" shadow-slate-600 shadow-md rounded-md sm:p-2 bg-[#dedede]"><a className="inline-block py-2 mx-3 text-2xl sm:py-0 text-blue-500" href="https://www.linkedin.com/in/steve-mafoa-07a513179/" target="_blank"><FaLinkedin /></a></li>
            <li className=" shadow-slate-600 shadow-md rounded-md sm:p-2 bg-[#dedede] mx-12"><a className="inline-block py-2 mx-3 text-2xl sm:py-0 text-black" href="https://github.com/MafoaSteve9" target="_blank"><FaGithub /></a></li>
-           <li className=" shadow-slate-600 shadow-md rounded-md sm:p-2 bg-[#dedede]"><a className="inline-block py-2 mx-3 text-2xl sm:py-0 text-black" href="/CV-Steve-full-stack.pdf" download><IoMdDownload /></a></li>
+           <li className=" shadow-slate-600 shadow-md rounded-md sm:p-2 bg-[#dedede]"><a className="inline-block py-2 mx-3 text-2xl sm:py-0 text-black" href="/CV-Steve.pdf" download><IoMdDownload /></a></li>
            
        </ul>
    </nav>
