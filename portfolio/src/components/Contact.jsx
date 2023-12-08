fdc474aaaa7902c1ba41d9696bdea903746fc0f6
Mafoa <steve_mafoa@outlook.com>
2023-12-08 17:46:54 +0100

    Creation du component Contact

---
+++ portfolio/src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import { createPortal } from "react-dom"
+import ModalContent from "./ModalContent"
+
+export default function Contact() {
+    const [selectedTab, setSelectedTab] = useState(0)
+    const [showModal, setShowModal] = useState(false)
+
+  return (
+    <div className="flex justify-center p-4">
+      <button 
+        onClick={() => setShowModal(true)}
+        className="mt-2 py-3 px-10 cursor-pointer text-sm rounded bg-[#3a6ed6] text-white hover:bg-blue-700 mb-8">
+          Me contacter
+          </button>
+          {showModal && createPortal(<ModalContent closeModal={() => setShowModal(false)}/>, document.body)}
+      </div>
+      
+  )
+}
